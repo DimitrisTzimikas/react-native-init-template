8c0b6560f1fb4a7fd0d9a1dc94167de5b7aa5ed7
TZIMIKAS Dimitris <tzimikasd@gmail.com>
2020-12-03 17:05:03 +0200

    update

--- init_files/src/navigation/index.js
+++ init_files/src/navigation/index.js
@@ -1,12 +1,16 @@
 /* Modules */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {useReduxDevToolsExtension} from '@react-navigation/devtools';
 /* Local Files */
 import BottomTabs from './tabs/Bottom.tab.js';
 
 const AppNavigation = () => {
+  const navigationRef = React.useRef();
+  useReduxDevToolsExtension(navigationRef);
+
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <BottomTabs />
     </NavigationContainer>
   );
